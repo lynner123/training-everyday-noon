d0242a5a2e71797cf178591d26e1b79b803b8278
lyn <shandonglyn@163.com>
2017-08-11 14:20:06 +0800

    '0811'

--- test_0811.js
+++ test_0811.js
@@ -1,3 +1,24 @@
-/*日期：2017/08/08
- 题目：
+/*日期：2017/08/11
+ 题目：给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。
+ function XXX(str,val){}
  */
+
+function chainMake(str, val) {
+    let cut = [];
+    let array = str.split("->");
+
+    for (let item of array) {
+        if (item != val) {
+            cut.push(item);
+        }
+    }
+
+    let result = '' + cut[0];
+    for (let i = 1; i < cut.length; i++) {
+        result += '->' + cut[i];
+    }
+    return result;
+}
+var chain = '1->2->3->3->4->5->3';
+var val = 3;
+console.log(chainMake(chain, val));
