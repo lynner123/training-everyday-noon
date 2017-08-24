9fee8f03656133339b742ba34d0c973f15efa87e
lynner123 <shandonglyn@163.com>
2017-08-24 14:19:49 +0800

    '0824/14:12'

--- test_0824.js
+++ test_0824.js
@@ -9,16 +9,19 @@ leftpad(‘hello’,20,‘1’)，就要返回’111111111111111hello’
 第三个：用来拼接的字符串
 */
 let arr1 = ['hello',20,'1'];
-let arr2 = ['hello',21,'12'];
+let arr2 = ['hello',20,'12'];
+let arr3 = ['hello',20,'123'];
 
 function left_pad(arr) {
     let str = '';
-    for (let i = 0; i < parseInt((arr[1]-arr[0].length)/arr[2].length); i++) {
+    for (let i = 0; i < Math.ceil((arr[1]-arr[0].length)/arr[2].length); i++) {
         str += arr[2];
     }
+
+    str = str.slice(0,arr[1]-arr[0].length);
     return str+arr[0];
 }
 
 console.log('arr1=' + arr1+',返回值为：\n '+ left_pad(arr1));
 console.log('arr2=' + arr2+',返回值为：\n '+ left_pad(arr2));
-
+console.log('arr3=' + arr3+',返回值为：\n '+ left_pad(arr3));
